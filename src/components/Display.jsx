4ca9c751917b7070bdd2c9d4d8b14cf562f16c30
Ana <anapedrabcn@gmail.com>
2025-01-13 00:10:57 +0100

    final app

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,12 +1,32 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
 import DisplayHome from "./DisplayHome";
+import { Route, Routes, useLocation } from "react-router-dom";
+import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/assets";
 
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  let isAlbum = location.pathname.includes("album");
+  let albumId = isAlbum ? location.pathname.slice(-1) : "";
+  let bgColor = albumsData[Number(albumId)].bgColor;
+
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = "#121212";
+    }
+  });
+
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
+    <div
+      ref={displayRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import PlayerContextProvider from "./context/PlayerContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <PlayerContextProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </PlayerContextProvider>
+  </BrowserRouter>
 );
